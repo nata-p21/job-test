c52dece5c670e6839ef0060852735b3f3d7adff6
vagrant <vagrant@homestead>
2018-06-22 21:43:16 +0000

    initial

---
+++ task1/index.php
@@ -0,0 +1,25 @@
+<?php
+//echo phpinfo();
+$inputFile = __DIR__ . '/input.txt';
+$outputFile = __DIR__ . '/output.txt';
+
+if (file_exists($inputFile)) {
+    $text = file_get_contents($inputFile);
+
+    $position = 0;
+    $text = preg_replace_callback('/\w+/u', function ($matches) use (&$position) {
+        $position++;
+        list($replacement) = $matches;
+        if ($position % 3 == 0 && $position % 5 == 0) {
+            $replacement = 'ПЯТНАДЦАТЬ';
+        } elseif ($position % 3 == 0) {
+            $replacement = 'ТРИ';
+        } elseif ($position % 5 == 0) {
+            $replacement = 'ПЯТЬ';
+        }
+        return $replacement;
+    }, $text);
+
+    file_put_contents($outputFile, $text);
+    
+}
